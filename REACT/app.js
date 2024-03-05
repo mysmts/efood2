e49f0aae66557e9e29c13920d83eed8b4a0b708a
Maysa Matos <maysaestudo@gmail.com>
2024-03-04 21:36:21 -0300

    all changes

---
+++ REACT/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import IMCCalculator from "./IMCCALCULATOR";
+
+function App() {
+  return (
+    <div className="App">
+      <IMCCalculator />
+    </div>
+  );
+}
+
+export default App;
