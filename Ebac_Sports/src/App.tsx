e2d2ef3f57f9c4f9abf7ec8dfea1f092944bc915
Maysa Matos <maysaestudo@gmail.com>
2024-04-28 16:57:01 -0300

    all changes

--- Ebac_Sports/src/App.tsx
+++ Ebac_Sports/src/App.tsx
@@ -1,32 +1,26 @@
-import React from 'react';
-import Header from './components/Header'
-import Produtos from './containers/Produtos'
-
-import { Provider } from 'react-redux'
-import { GlobalStyle } from './store/styles'
-
-import { store } from './store'
-
-export type Produto = {
-  id: number
-  nome: string
-  preco: number
-  imagem: string
-}
+// App.tsx
+import React, { useState } from 'react';
+import { Provider } from 'react-redux';
+import Header from './components/header';
+import Produtos from './containers/Produtos';
+import { GlobalStyle } from './store/styles';
+import { store } from './store';
+import { Produto } from './components/Produto/types';
 
 function App() {
-  const [favoritos, setFavoritos] = useState<Produto[]>([])
+  const [favoritos, setFavoritos] = useState([] as Produto[]);
 
   function favoritar(produto: Produto) {
     if (favoritos.find((p) => p.id === produto.id)) {
-      const favoritosSemProduto = favoritos.filter((p) => p.id !== produto.id)
-      setFavoritos(favoritosSemProduto)
+      const favoritosSemProduto = favoritos.filter((p) => p.id !== produto.id);
+      setFavoritos(favoritosSemProduto);
     } else {
-      setFavoritos([...favoritos, produto])
+      // Adicione a propriedade 'favoritos' ao produto antes de adicioná-lo aos favoritos
+      const produtoComFavoritos = { ...produto, favoritos: true };
+      setFavoritos([...favoritos, produtoComFavoritos]);
     }
   }
   
-
   return (
     <Provider store={store}>
       <GlobalStyle />
@@ -35,7 +29,10 @@ function App() {
         <Produtos favoritos={favoritos} favoritar={favoritar} />
       </div>
     </Provider>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+
---
+++ Ebac_Sports/src/components/Produto/types.ts
@@ -0,0 +1,9 @@
+// types.ts
+export type Produto = {
+    id: number;
+    nome: string;
+    preco: number;
+    imagem: string;
+    favoritos: boolean; // Propriedade favoritos é opcional
+  };
+  
