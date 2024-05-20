7cb9c9c437296ebd04027c4c8febff5abf4e50ca
Maysa Matos <maysaestudo@gmail.com>
2024-05-19 22:35:44 -0300

    all changes

--- PROJETO05/src/App.tsx
+++ PROJETO05/src/App.tsx
@@ -1,17 +1,28 @@
-import { Provider } from 'react-redux'
-import ListaDeContatos from './Containers/ListaDeContatos'
-import store from './store'
-import EstiloGlobal, { Container } from './styles'
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
-    <Provider store={store}>
-      <EstiloGlobal />
-      <Container>
-        <ListaDeContatos />
-      </Container>
-    </Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   )
 }
 
 export default App
+
+export {}
