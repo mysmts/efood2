6b2c5f57aaa59c83708c59b02480ba3338e97301
Maysa Matos <maysaestudo@gmail.com>
2024-05-20 23:45:32 -0300

    all changes

--- PROJETO05/src/App.tsx
+++ PROJETO05/src/App.tsx
@@ -1,28 +1,18 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Provider } from 'react-redux'
+import ListaDeContatos from './containers/ListadeContatos/index'
+import store from './store'
+import EstiloGlobal from './containers/ListadeContatos'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <EstiloGlobal />
+      <ListaDeContatos />
+    </Provider>
   )
 }
 
 export default App
+// App.tsx
 
-export {}
+// Remova o caractere de quebra de linha na linha 16
