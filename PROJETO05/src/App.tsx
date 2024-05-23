7b7801563b26630b68c7a6435fac0ff4a87fe341
Maysa Matos <110046933+mysmts@users.noreply.github.com>
2024-05-23 08:42:57 -0300

    Update App.tsx

--- PROJETO05/src/App.tsx
+++ PROJETO05/src/App.tsx
@@ -1,12 +1,10 @@
 import { Provider } from 'react-redux'
 import ListaDeContatos from './containers/ListadeContatos/index'
 import store from './store'
-import EstiloGlobal from './containers/ListadeContatos'
 
 function App() {
   return (
     <Provider store={store}>
-      <EstiloGlobal />
       <ListaDeContatos />
     </Provider>
   )
