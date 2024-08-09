76eca6d7e0e7cd938e0c1edf72e4f2b4e1cd54f7
Maysa Matos <110046933+mysmts@users.noreply.github.com>
2024-08-09 00:02:11 -0300

    Delete Bootstrap Avançado directory

--- Bootstrap Avançado/js/main.js
+++
@@ -1,7 +0,0 @@
-main.js
-
-// Inicie a máscara para o campo de telefone
-var telefoneInput = document.getElementById("telefone");
-var telefoneMask = new IMask(telefoneInput, {
-  mask: "(00) 0 0000-0000",
-});
