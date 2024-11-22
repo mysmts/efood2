18ae866534636a55e5d5cb926fc9b0b8e3b29a45
Maysa Matos <110046933+mysmts@users.noreply.github.com>
2024-11-22 09:16:24 -0300

    Update routes.tsx

--- efood2/src/routes.tsx
+++ efood2/src/routes.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
-import Perfil from './pages/Perfil';
+import Home from './Pages/Home';
+import Perfil from './Pages/Perfil';
 
 const Rotas = () => (
   <Routes>
